9fdaf254bd8d93bcee5eade29019501e535bfa2e
tintinsnowy <yangxiaoli1994@gmail.com>
2018-10-31 17:09:16 +0100

    successfully start the docker

--- basic-network/generate.sh
+++ basic-network/generate.sh
@@ -6,36 +6,57 @@
 #
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
+# export PATH=/home/sherry/Code/fabric-samples/bin:$PATH
 CHANNEL_NAME=mychannel
 
 # remove previous crypto material and config transactions
-rm -fr config/*
+rm -fr channel-artifact/*
 rm -fr crypto-config/*
 
 # generate crypto material
-../bin/cryptogen generate --config=./crypto-config.yaml
+cryptogen generate --config=./crypto-config.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material..."
   exit 1
 fi
 
 # generate genesis block for orderer
-../bin/configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+export PATH=/home/sherry/Code/fabric-samples/bin:$PATH
+configtxgen -profile FourOrgOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+export CHANNEL_NAME=mychannel
+configtxgen -profile FourOrgChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile FourOrgChannel -outputAnchorPeersUpdate ./channel-artifacts/OperatorMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OperatorMSP
 if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org1MSP..."
+  echo "Failed to generate anchor peer update for OperatorMSP..."
+  exit 1
+fi
+
+configtxgen -profile FourOrgChannel -outputAnchorPeersUpdate ./channel-artifacts/SupplierMSPanchors.tx -channelID $CHANNEL_NAME -asOrg SupplierMSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for SupplierMSP..."
+  exit 1
+fi
+
+configtxgen -profile FourOrgChannel -outputAnchorPeersUpdate ./channel-artifacts/DistributorMSPanchors.tx -channelID $CHANNEL_NAME -asOrg DistributorMSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for DistributorMSP..."
+  exit 1
+fi
+
+configtxgen -profile FourOrgChannel -outputAnchorPeersUpdate ./channel-artifacts/RetailerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg RetailerMSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for RetailerMSP..."
   exit 1
 fi
--- basic-network/start.sh
+++ basic-network/start.sh
@@ -12,7 +12,9 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+CHANNEL_NAME=$CHANNEL_NAME docker-compose -f docker-compose-cli.yaml up -d
+
+#docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -20,7 +22,29 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
+# TEST
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Operator.example.com/users/Admin@Operator.example.com/msp
+CORE_PEER_ADDRESS=peer0.Operator.example.com:7051
+CORE_PEER_LOCALMSPID="OperatorMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Operator.example.com/peers/peer0.Operator.example.com/tls/ca.crt
+
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Supplier.example.com/users/Admin@Supplier.example.com/msp
+CORE_PEER_ADDRESS=peer0.Supplier.example.com:7051
+CORE_PEER_LOCALMSPID="SupplierMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Supplier.example.com/peers/peer0.Supplier.example.com/tls/ca.crt
+
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Distributor.example.com/users/Admin@Distributor.example.com/msp
+CORE_PEER_ADDRESS=peer0.Supplier.example.com:7051
+CORE_PEER_LOCALMSPID="DistributorMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/Distributor.example.com/peers/peer0.Distributor.example.com/tls/ca.crt
+
+
+export CHANNEL_NAME=mychannel
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
 # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=OrdererMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" 
+
+peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=OrdererMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
